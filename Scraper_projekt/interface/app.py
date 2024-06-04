8cbd6704a204f14971dad3abda18b52525861fb8
Oskar Skowroński <97964979+OskarSko@users.noreply.github.com>
2024-06-04 15:10:05 +0200

    Add files via upload

---
+++ Scraper_projekt/interface/app.py
@@ -0,0 +1,72 @@
+import json
+import time
+import docker
+from flask import Flask, request, redirect, render_template, jsonify, url_for
+from pymongo import MongoClient
+import requests
+
+app = Flask(__name__)
+client = docker.from_env()
+
+def get_db():
+    client = MongoClient('mongodb://root:example@mongo:27017/')
+    db = client['test_database']
+    return db
+
+@app.route('/')
+def index():
+    with open('links.json', 'r') as f:
+        links = json.load(f)
+    brands = list(links.keys())
+    
+    db = get_db()
+    collection = db['car_collection']
+    cars = list(collection.find({}, {'_id': 0}))
+    return render_template("index.html", brands=brands, cars=cars)
+
+@app.route('/cars')
+def cars():
+    db = get_db()
+    collection = db['car_collection']
+    cars = list(collection.find({}, {'_id': 0}))
+    return jsonify(cars)
+
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('query', '')
+    db = get_db()
+    collection = db['car_collection']
+    
+    # Używamy wyrażenia regularnego do wyszukiwania samochodów po nazwie
+    cars = list(collection.find({'nazwa': {'$regex': query, '$options': 'i'}}, {'_id': 0}))
+    
+    return jsonify(cars)
+
+
+@app.route('/scrape', methods=['GET', 'POST'])
+def scrape():
+    if request.method == 'POST':
+        brand = request.form.get('brand')
+        with open('links.json', 'r') as f:
+            links = json.load(f)
+        if brand in links:
+            link = links[brand]
+            container = client.containers.get('projekt_scraper-scraper_engine-1')
+            container.restart()  # Restart container to ensure the latest code is running
+            time.sleep(5)  # Give some time for the container to restart
+            response = requests.post('http://projekt_scraper-scraper_engine-1:5001/scrape', json={'url': link})
+            if response.status_code == 200:
+                return redirect(url_for('index'))
+            else:
+                return f"Scraping failed: {response.text}", 500
+        else:
+            return "Brand not found", 404
+    else:
+        with open('links.json', 'r') as f:
+            links = json.load(f)
+        brands = list(links.keys())
+        return render_template('scrape.html', brands=brands)
+        return "Brand not found", 404
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=5000)
